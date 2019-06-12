88890d285fb10d3c2d993e3d3f5020ca7b7772e7
Shantay <shantaygrady@gmail.com>
2019-06-12 18:34:27 -0400

    Updates to article

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,28 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(articleEl) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = articleEl.querySelector('.article');
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = articleEl.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = "expand";
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this))
   }
-
+  
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.expandButton.classList.toggle("hide-btn");
+    this.colaspeButton.classList.toggle("hide-btn");
+    this.articleEl.classList.toggle("toggle-on")
   }
+
 }
+ // Creates close button--------------------------
+ closeButton = articles.createElement('span')
 
 /* START HERE: 
 
@@ -26,7 +32,10 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
-articles.forEach( (artFrame) =>{
-  return new Article(artFrame);
+const articles = document.querySelectorAll('.article');
+
+
+
+articles.forEach( (articleEl) =>{
+  return new Article(articleEl);
 })
