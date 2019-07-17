ab1866651edd3d62abb17e593ea053e6f03d8910
Shantay Grady <shantaygrady@gmail.com>
2019-07-17 02:24:06 -0400

    Imports less. Setup menu files

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,8 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+const menuComponent = document.createElement('div');
+menuComponent.className = "menu";
+const ulList = document.createElement('ul');
+menuComponent.appendChild(ulList);
+ulList.appendChild(menuItems);
